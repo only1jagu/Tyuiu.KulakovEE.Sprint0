0af0d1ac267c5c536f3da40305e1471ee8862728
Егор Кулаков <egorkakylakov2000@mail.ru>
2023-09-08 21:04:37 +0500

    Task2

---
+++ Tyuiu.KulakovEE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KulakovEE.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KulakovEE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Егор";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Егор", res);
+        }
+    }
+}
---
+++ Tyuiu.KulakovEE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KulakovEE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KulakovEE.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вызов класса DataService и метода GetMessage
+            // из библиотеки Tyuiu.KulakovEE.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Егор"));
+            Console.ReadKey();
+        }
+    }
+}
